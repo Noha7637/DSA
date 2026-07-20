75b93ebcad09d1031cc46907d7027c5f164021e0
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-20 20:02:40 +0300

    Time: 39 ms (24.20%) | Memory: 12.3 MB (92.91%) - LeetSync

--- 2752-sum-multiples/sum-multiples.py
+++ 2752-sum-multiples/sum-multiples.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         sum = 0
-        for i in range(1, n+1):
+        for i in range(3, n+1):
             if i%3==0 or i%5==0 or i%7==0:
                 sum = sum + i
         return sum
