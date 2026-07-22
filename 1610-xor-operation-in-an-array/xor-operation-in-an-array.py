70df3184df9a626c61665e23dca8962981102045
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-22 11:02:17 +0300

    Time: 3 ms (11.41%) | Memory: 12.3 MB (58.45%) - LeetSync

---
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def xorOperation(self, n, start):
+        """
+        :type n: int
+        :type start: int
+        :rtype: int
+        """
+        nums = [start + 2*i for i in range(n)]
+        xor = 0
+        for j in range(len(nums)):
+            if j == 0:
+                xor = nums[j]  
+            else:
+                xor = xor ^ nums[j]  
+        return xor    
+
+        
