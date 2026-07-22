d8cd038eefa22dfb7af0b1a3d073c7fb2c11701b
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-22 11:06:21 +0300

    Time: 0 ms (100.00%) | Memory: 12.3 MB (93.08%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -5,13 +5,20 @@ class Solution(object):
         :type start: int
         :rtype: int
         """
-        nums = [start + 2*i for i in range(n)]
+        """ nums = [start + 2*i for i in range(n)]
         xor = 0
         for j in range(len(nums)):
             if j == 0:
                 xor = nums[j]  
             else:
                 xor = xor ^ nums[j]  
-        return xor    
+        return xor """
+        nums = [start + 2*i for i in range(n)]
+        xor = 0
+        for num in nums:
+            xor = xor ^ num
+        return xor
+
+
 
         
