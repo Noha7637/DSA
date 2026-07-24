a9e7cad0e886cc318527e32841ec98c6046444e0
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-24 22:56:20 +0300

    Time: 4 ms (12.32%) | Memory: 12.5 MB (18.22%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -5,9 +5,8 @@ class Solution(object):
         :rtype: bool
         """
         arr_sentence = [i for i in sentence]
-        new_sentence = list(sorted(set(arr_sentence)))
-        alphabet = [chr(i).lower() for i in range(65,91)]
-        if alphabet == new_sentence:
+        new_sentence = set(arr_sentence)
+        if len(new_sentence) == 26:
             return True
         else:
             return False
