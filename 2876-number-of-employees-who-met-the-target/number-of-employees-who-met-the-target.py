dd53e6905c4dc3c074085b789f839df6e0d6b854
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-24 20:49:09 +0300

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.47%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        """
+        :type hours: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return len([i for i in hours if i>=target])
+
+        
