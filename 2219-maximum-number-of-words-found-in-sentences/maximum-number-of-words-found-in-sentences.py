02e62ae469464c99a013cdf3891ed2b587ce9c84
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-24 22:05:44 +0300

    Time: 3 ms (42.60%) | Memory: 12.3 MB (65.54%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def mostWordsFound(self, sentences):
+        """
+        :type sentences: List[str]
+        :rtype: int
+        """
+        return max([len(sentence.split()) for sentence in sentences])
+
+        
