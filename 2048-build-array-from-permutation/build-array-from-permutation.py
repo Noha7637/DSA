dafd317e6811198b2de8f1288837582b3985c78a
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-25 22:54:37 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (75.05%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        arr = [nums[i] for i in nums]
+        return arr
+        
