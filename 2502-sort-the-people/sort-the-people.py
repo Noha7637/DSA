6a2ee15f04aa053de116a6e3ac091cdf21604ebe
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-27 10:33:45 +0300

    Time: 1470 ms (5.18%) | Memory: 12.6 MB (86.83%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -5,18 +5,17 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[str]
         """
-        dictionary = {heights[i]:names[i] for i in range(len(names))}
         counter = 0
         while True:
             for i in range(len(heights)-1):
                 if heights[i+1]>heights[i]:
                     heights[i], heights[i+1] = heights[i+1], heights[i]
+                    names[i], names[i+1] = names[i+1], names[i]
                     counter += 1
             if counter == 0:
                 break
             counter = 0
-        arr = [dictionary[i] for i in heights]
-        return arr
+        return names
             
             
 
