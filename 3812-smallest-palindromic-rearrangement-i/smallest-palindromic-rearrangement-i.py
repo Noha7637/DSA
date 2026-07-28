1c5dbcd43bb6b873ecd3d0b625105858ba8b392f
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-28 07:28:12 +0300

    Time: 342 ms (82.35%) | Memory: 14.2 MB (32.35%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def smallestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        half1 = ""
+        half2 = ""
+        total = ""
+        if len(s)%2 == 0:
+            half1 = "".join(sorted(s[0:len(s)/2]))
+            half2 = half1[::-1]
+            total = half1  + half2
+        elif len(s)%2 != 0:
+            half1 = "".join(sorted(s[0:len(s)//2]))
+            half2 = half1[::-1]
+            total = half1 + s[len(s)//2] + half2
+        return min([total,s])
+
+        
+            
+            
+            
+
+        
+
+
+
+
+
+        
