0e82ce5c0bcb3c74e5fb531f924e74c8c03566df
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-30 21:35:44 +0300

    Time: 0 ms (100.00%) | Memory: 12.3 MB (59.62%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -4,19 +4,16 @@ class Solution(object):
         :type word: str
         :rtype: int
         """
-        set1 = list(set(word))
         count = 0
-        for i in range(len(word)):
-            for j in range(len(set1)):
-                if word[i] == set1[j]:
-                    if j<8:
-                        count+=1
-                    elif j<16:
-                        count+=2
-                    elif j<24:
-                        count+=3
-                    elif j<26:
-                        count+=4
+        for j in range(len(word)):
+            if j<8:
+                count+=1
+            elif j<16:
+                count+=2
+            elif j<24:
+                count+=3
+            elif j<26:
+                count+=4
         return count
                     
         
