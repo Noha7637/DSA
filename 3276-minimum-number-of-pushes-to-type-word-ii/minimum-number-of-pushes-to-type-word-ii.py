1a04ba4848bb3ae306bc06b3fe8820d38755b953
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-07-31 10:36:22 +0300

    Time: 942 ms (5.35%) | Memory: 20.1 MB (23.08%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -2,19 +2,17 @@ class Solution:
     def minimumPushes(self, word: str) -> int:
         count =  Counter(word)
         sorted_count = dict(sorted(count.items(), key=lambda item: item[1]))
-        arr = []
-        for i in sorted_count.keys():
-                arr.append(i)
-        arr2 = arr[::-1]
+        arr = list(sorted_count.keys())
+        arr.reverse()
         mycounter = 0
         for i in range(len(word)):
-            if word[i] in arr2[0:8]:
+            if word[i] in arr[0:8]:
                 mycounter +=1
-            elif word[i] in arr2[8:16]:
+            elif word[i] in arr[8:16]:
                 mycounter +=2
-            elif word[i] in arr2[16:24]:
+            elif word[i] in arr[16:24]:
                 mycounter +=3
-            elif word[i] in arr2[24:26]:
+            elif word[i] in arr[24:26]:
                 mycounter +=4
         return mycounter
         
