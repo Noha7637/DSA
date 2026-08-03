6d940581d1e66fc00b8f171a44ad8409d51f31a8
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-08-03 11:54:56 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (78.31%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,13 +3,13 @@ class Solution:
         ptr1 = 0
         for ptr2 in range(1, len(nums)):
             if nums[ptr1]<nums[ptr2]:
-                nums[ptr1+1], nums[ptr2] = nums[ptr2], nums[ptr1+1]
+                nums[ptr1+1]= nums[ptr2]
                 ptr1+=1
-        return len(nums[0:ptr1+1])
+        return ptr1 + 1
        
             
         
             
             
 
-        
+    
