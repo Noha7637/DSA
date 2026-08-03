31724ca26c0dcec2870058c7d5da260ce88c1a83
Noah Nathenael <noah.ugr-9823-17@aau.edu.et>
2026-08-03 13:30:02 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.68%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -3,18 +3,17 @@ class Solution:
         arr = []
         arrout = []
         for i in range(numRows):
-            if i==0:
+            '''if i==0:
                 arrout.append([1])
             elif i==1:
-                arrout.append([1,1])
-            else:
-                for j in range(i+1):
-                    if j == 0 or j == i:
-                        arr.append(1)
-                    else:
-                        arr.append(arrout[i-1][j-1] + arrout[i-1][j])
-                arrout.append(arr)
-                arr = []
+                arrout.append([1,1])'''
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    arr.append(1)
+                else:
+                    arr.append(arrout[i-1][j-1] + arrout[i-1][j])
+            arrout.append(arr)
+            arr = []
         return arrout
 
                     
