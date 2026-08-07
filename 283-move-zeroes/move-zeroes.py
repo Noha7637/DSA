f81d8148dd6cac5842c71bd96329ea77f60e23c7
Noha.N <noah.ugr-9823-17@aau.edu.et>
2026-08-07 13:33:01 +0300

    Time: 3 ms (81.60%) | Memory: 20.4 MB (89.45%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,17 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        m = []
-        ptr = 0
-        while ptr<len(nums):
-            if nums[ptr]==0:
-                nums.pop(ptr)
-                m.append(0)
-            else:
-                ptr+=1
-        for i in range(len(m)):
-            nums.append(0)
-        
+        holder = 0
+        seeker = 0
 
+        while seeker < len(nums):
+            if nums[seeker] != 0:
+                nums[seeker], nums[holder] = nums[holder], nums[seeker]
+                holder += 1
+            seeker += 1
 
         
