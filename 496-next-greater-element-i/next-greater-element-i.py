ee8dc0153ca3910e9e8db529940e615ed64d1f69
Noha.N <noah.ugr-9823-17@aau.edu.et>
2026-08-08 09:21:21 +0300

    Time: 21 ms (17.94%) | Memory: 19.2 MB (92.52%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,21 +1,19 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        arr = [] 
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i]==nums2[j]:
-                    if j == len(nums2)-1:
-                        arr.append(-1)
+        ptr1 = 0
+        indx = 0
+        arr = []
+        while ptr1 < len(nums1):
+            indx = nums2.index(nums1[ptr1])
+            if indx == len(nums2)-1:
+                arr.append(-1)
+            else:
+                for i in range(indx+1, len(nums2)):
+                    if nums2[i]>nums2[indx]:
+                        arr.append(nums2[i])
                         break
-                    elif nums2[j+1]>nums2[j]:
-                        arr.append(nums2[j+1])
-                        break
-                    else:
-                        for k in range(j+1, len(nums2)):
-                            if nums2[k]>nums2[j]:
-                                arr.append(nums2[k])
-                                break
-                        else:
-                            arr.append(-1)
-
-        return arr
+                else:
+                    arr.append(-1)
+            ptr1+=1
+        return arr   
+    
