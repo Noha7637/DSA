db67ee3aa711d4cf726ad3a8be90ebea924dd116
Noha.N <noah.ugr-9823-17@aau.edu.et>
2026-08-21 21:18:32 +0300

    Time: 3 ms (4.63%) | Memory: 19.5 MB (6.84%) - LeetSync

---
+++ 476-number-complement/number-complement.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        number = bin(num)[2:]
+        complement = []
+        for i in range(len(number)):
+            if number[i]=="0":
+                complement.append(1)
+            elif number[i]=="1":
+                complement.append(0)
+        complement.reverse()
+        total = 0
+        for i in range(len(complement)):
+            total+= complement[i] * 2**i
+        return total
+            
+        
+        
+        
+        
+
+            
+        
